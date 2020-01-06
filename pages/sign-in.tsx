bdb38bed69689a64280e327415a3d8fd383f5866
Ali Zahid <ali.zahid@live.com>
2020-01-06 12:56:27 +0400

    Allow setting name for new users

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -8,7 +8,9 @@ import { firebase, redirect } from '../lib'
 
 const SignIn: NextPage = () => {
   const [error, setError] = useState('')
+  const [isNew, setIsNew] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [name, setName] = useState('')
 
   return (
     <>
@@ -22,26 +24,71 @@ const SignIn: NextPage = () => {
         <section className="bg-primary-dark rounded-lg p-8 w-signin">
           <h1 className="text-5xl font-semibold">Hello</h1>
           {error && <FormMessage message={error} type="error" />}
-          <button
-            className="mt-40 w-full"
-            onClick={async () => {
-              setError('')
-              setLoading(true)
+          {isNew && (
+            <form
+              className="mt-40"
+              onSubmit={async event => {
+                event.preventDefault()
 
-              try {
-                const provider = new firebase.auth.GoogleAuthProvider()
+                if (name) {
+                  setError('')
+                  setLoading(true)
 
-                await firebase.auth().signInWithPopup(provider)
+                  try {
+                    await firebase.auth().currentUser?.updateProfile({
+                      displayName: name
+                    })
 
-                Router.replace('/')
-              } catch ({ message }) {
-                setError(message)
-              } finally {
-                setLoading(false)
-              }
-            }}>
-            {loading ? <Spinner /> : 'Sign in with Google'}
-          </button>
+                    Router.replace('/')
+                  } catch ({ message }) {
+                    setError(message)
+                  } finally {
+                    setLoading(false)
+                  }
+                }
+              }}>
+              <label>
+                <span>Pick a username</span>
+                <input
+                  className="bg-primary w-full"
+                  onChange={event => setName(event.target.value)}
+                  placeholder="Username"
+                  required
+                  type="text"
+                  value={name}
+                />
+              </label>
+              <button>{loading ? <Spinner /> : 'Sign up'}</button>
+            </form>
+          )}
+          {!isNew && (
+            <button
+              className="mt-40 w-full"
+              onClick={async () => {
+                setError('')
+                setLoading(true)
+
+                try {
+                  const provider = new firebase.auth.GoogleAuthProvider()
+
+                  const {
+                    additionalUserInfo
+                  } = await firebase.auth().signInWithPopup(provider)
+
+                  if (additionalUserInfo?.isNewUser) {
+                    setIsNew(true)
+                  } else {
+                    Router.replace('/')
+                  }
+                } catch ({ message }) {
+                  setError(message)
+                } finally {
+                  setLoading(false)
+                }
+              }}>
+              {loading ? <Spinner /> : 'Sign in with Google'}
+            </button>
+          )}
         </section>
       </main>
 
